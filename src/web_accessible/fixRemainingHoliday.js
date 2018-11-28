d8334dcf18d5e8daba9bddef94ec869d0e5bf513
Byran Wills-Heath <byran@adgico.co.uk>
2018-11-28 00:51:03 +0000

    Change variables to constants in injected script

--- src/web_accessible/fixRemainingHoliday.js
+++ src/web_accessible/fixRemainingHoliday.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
-    var elements = $('.TimeOffWidget__type-available');
-    let timeOffElement = elements.first();
+    const elements = $('.TimeOffWidget__type-available');
+    if(elements.length > 0) {
+        const timeOffElement = elements.first();
 
-    let current = new Date();
-    let year = current.getFullYear();
-    var data = {
-        endDate: '31/12/' + year,
-        timeOffType: '1',
-        employeeId: window.common.employeeId
-    };
-    
-    $.ajax({
-        type: "GET",
-        data: data,
-        url: "/time_off/calculator/calculate",
-        dataType: 'json',
-        success: function(values) {
-            var holidayHtml = '<div id="FixedTime">' + values.total + '</div>'
+        const current = new Date();
+        const year = current.getFullYear();
+        const data = {
+            endDate: '31/12/' + year,
+            timeOffType: '1',
+            employeeId: window.common.employeeId
+        };
 
-            timeOffElement.html(holidayHtml);
-        }
-    });
+        $.ajax({
+            type: "GET",
+            data: data,
+            url: "/time_off/calculator/calculate",
+            dataType: 'json',
+            success: function(values) {
+                const holidayHtml = '<div id="FixedTime">' + values.total + '</div>'
+
+                timeOffElement.html(holidayHtml);
+            }
+        });
+    }
 });
