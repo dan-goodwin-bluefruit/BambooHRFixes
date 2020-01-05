9999232185d0a2c52afb641d195a82b27339a7e2
Byran Wills-Heath <byran@adgico.co.uk>
2020-01-05 23:52:03 +0000

    Update 'Full Calendar' navigation for new BambooHR layout

--- src/web_accessible/navigationChanges.js
+++ src/web_accessible/navigationChanges.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-    const navigation = $('#nav-tabs');
+    const navigation = $('.SiteNavigation');
 
     if(navigation.length > 0) {
         var link = document.createElement('a');
+        link.classList = 'SiteNavigation__link';
+        link.setAttribute('data-text', 'Full Calendar');
         link.href = '/calendar';
+        link.setAttribute('data-bi-id', 'main-nav-fullcalendar-tab');
         link.text = 'Full Calendar';
-        link.setAttribute('aria-labelledby', 'nav-tabs');
 
-        var listItem = document.createElement('li');
-        listItem.appendChild(link);
-
-        navigation.append(listItem);
+        navigation.append(link);
     }
 });
